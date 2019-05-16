ddbaf762ff2d346a448238678b1c9d90bed7eb00
Goessi <qinj1994@163.com>
2019-05-16 15:46:31 +0200

    more

--- cppOnline/OverloadingOperators/MystringInExsertion/Mystring.h
+++ cppOnline/OverloadingOperators/MystringInExsertion/Mystring.h
@@ -1,6 +1,6 @@
 #ifndef _MYSTRING_H_
 #define _MYSTRING_H_
-
+#include <iostream>
 class Mystring {
     friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
     friend std::istream &operator>>(std::istream &in, Mystring &rhs);
