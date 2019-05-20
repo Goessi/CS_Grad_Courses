5856683b8760ed6dfabbdf6562bcfdf261571d73
Goessi <qinj1994@163.com>
2019-05-20 14:55:23 +0200

    more

--- C++Primer/007class/simpleClass/ScreenAndWindow/Screen.cpp
+++ C++Primer/007class/simpleClass/ScreenAndWindow/Screen.cpp
@@ -28,3 +28,13 @@ Screen &Screen::move (pos r, pos c) {
 void Screen::some_member() const {
     ++access_ctr;
 }
+
+Screen &Screen::set(char c) {
+    contents[cursor] = c;
+    return *this;
+}
+
+Screen &Screen::set(pos r, pos col, char ch) {
+    contents[r*width + col] = ch;
+    return *this;
+}
